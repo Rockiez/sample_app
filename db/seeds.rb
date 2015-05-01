d6ce50e35238454e8032e9e94e49573df09b8bbe
Rockiez <simpi@foxmail.com>
2015-05-01 19:46:05 +0800

    Add account actibations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 User.create!(name:  "root",
               email: "root@admin.com",
-              password: "123456",password_confirmation: "123456",admin:true)
+              password: "123456",
+              password_confirmation: "123456",
+              admin:true,
+							activated: true,
+							activated_at: Time.zone.now)
 
 99.times do |n|
 	name  = Faker::Name.name
@@ -9,5 +13,7 @@ User.create!(name:  "root",
 	User.create!(name:  name,
 	             email: email,
 	             password:              password,
-	             password_confirmation: password)
+	             password_confirmation: password,
+	             activated: true,
+	             activated_at: Time.zone.now)
 end
