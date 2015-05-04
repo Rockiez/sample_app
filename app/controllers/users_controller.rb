01fea163bace41c71e0611219a3ea1bf7a8882cd
Rockiez <simpi@foxmail.com>
2015-05-04 15:12:34 +0800

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+	                                      :following, :followers]
 	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user, only: [:edit, :update]
 	before_action :admin_user, only: :destroy
@@ -10,8 +12,22 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@microposts = @user.microposts.paginate(page: params[:page])
 	end
+	def following
+		@title = "Following"
+		@user  = User.find(params[:id])
+		@users = @user.following.paginate(page: params[:page])
+		render 'show_follow'
+	end
 
-  def new
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+
+	def new
     @user = User.new
   end
   def create
